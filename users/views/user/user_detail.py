42182af01e29e9e5c1cf517b6de8a48523d88c8d
Alexander Suvorov <smartlegiondev@gmail.com>
2026-04-15 18:44:56 +0700

    feat: implement display of the last 5 user logins

--- users/views/user/user_detail.py
+++ users/views/user/user_detail.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from auth_logs.models import UserAuthLog
+
 
 @login_required
 def user_detail_view(request):
+    auth_logs = UserAuthLog.objects.filter(
+        user=request.user
+    ).order_by('-timestamp')[:5]
     context = {
-        'active_page': 'profile'
+        'active_page': 'profile',
+        'auth_logs': auth_logs,
     }
     return render(request, 'users/user_detail.html', context)
