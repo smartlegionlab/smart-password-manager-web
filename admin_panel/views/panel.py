1853d30d50b980345170a5bddb05bccb25d3bc3b
Alexander Suvorov <smartlegiondev@gmail.com>
2026-04-16 09:41:41 +0700

    style: improve UI

--- admin_panel/views/panel.py
+++ admin_panel/views/panel.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 from admin_panel.decorators.superuser import superuser_required
+from users.models import User
 
 
 @superuser_required
 def admin_panel_view(request):
-    return render(request, 'admin_panel/admin_panel.html', {})
+    user_count = User.objects.count()
+    context = {
+        'user_count': user_count,
+    }
+    return render(request, 'admin_panel/admin_panel.html', context)
--- smart_passwords/views/smart_password_create.py
+++ smart_passwords/views/smart_password_create.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 @login_required
 def smart_password_create_view(request):
     context = {
-        'active_page': 'create',
+        'active_page': 'manager',
     }
     return render(request, 'smart_passwords/smart_password_create.html', context)
